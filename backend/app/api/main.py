ca93e022dd3c743aa4827fb55d40d49c67806537
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2024-11-06 18:17:17 +0000

    Initial test function of FastAPI returning supabase data from the db.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.data.crud_example import get_test
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/api/test")
+async def test():
+    return get_test()
--- backend/app/data/crud_example.py
+++ backend/app/data/crud_example.py
@@ -7,3 +7,6 @@ from supabase import create_client, Client
 url: str = os.environ.get('SUPABASE_URL')
 key: str = os.environ.get('SUPABASE_KEY')
 supabase: Client = create_client(url, key)
+
+def get_test():
+    return supabase.from_("Test").select("*").execute()
