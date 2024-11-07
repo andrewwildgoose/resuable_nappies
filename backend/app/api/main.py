2008262078510d6989de647b725e2678774c36fa
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2024-11-07 16:05:57 +0000

    Tested adding data into table and signing up new user. Verification email not working.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from app.data.crud_example import get_test
+from fastapi import FastAPI, Form
+from app.data.crud_example import get_test, add_test_value, signup
+
 
 app = FastAPI()
 
@@ -11,3 +12,19 @@ async def root():
 @app.get("/api/test")
 async def test():
     return get_test()
+
+@app.post("/api/test/new")
+async def test(value: str = Form()):
+    try:
+        add_test_value(value)
+        return {"message": "Successfully added value"}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.post("/api/signup")
+async def signup(email: str = Form(), password: str = Form()):
+    try:
+        auth_response: str = signup(email, password)
+        return {"message": f"Successfully signed up: {auth_response}"}
+    except Exception as e:
+        return {"error": str(e)}
--- backend/app/data/crud_example.py
+++ backend/app/data/crud_example.py
@@ -10,3 +10,9 @@ supabase: Client = create_client(url, key)
 
 def get_test():
     return supabase.from_("Test").select("*").execute()
+
+def add_test_value(value: str):
+    return supabase.from_("Test").insert([{"value": value}]).execute()
+
+def signup(email: str, password: str):
+    return supabase.auth.sign_up(email, password)
